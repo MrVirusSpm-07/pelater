2bba8711bcf7ed398814bb60ebc0646a1519fa53
MrVirusSpm-07 <81134812+MrVirusSpm-07@users.noreply.github.com>
2021-06-01 21:04:19 +0700

    Add files via upload

---
+++ pemula.sh
@@ -0,0 +1,102 @@
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+sleep 1
+xdg-open https://youtube.com/channel/UCJcJ5ZARYowKSZzgPWSxpcQ
+    echo
+    echo $green
+figlet PELATER
+    sleep 2
+echo
+echo $blue".     # script ini untuk pemula # "
+echo $blue".          semoga bermanfaat"
+   echo
+sleep 1
+   echo $green"🎖️ AUTHOR : MR VIRUS SPM"
+   echo $green"🎖️ TEAM   : MAVIA TEKNOLOGI INDONESIA"
+   echo $green"🎖️ IG     : mvs.x_x"
+   echo $blue"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+   echo
+sleep 1
+echo $red"[01]"$yellow" PENDUKUNG WORK"
+echo $red"[02]"$yellow" AGAR TIDAK EROR"
+echo $red"[03]"$yellow" TOMBOL KANAN KIRI"
+echo $red"[04]"$yellow" TAMPILAN TERMUX"
+   echo $cyan
+read -p "+++++++> " grok
+if [ $grok = 1 ] || [ $grok = 01 ]
+then
+clear
+echo $green"# copyright by MR VIRUS SPM"
+echo $yellow"# sedang memulai"
+   sleep 1
+       pkg install git php curl -y
+       pkg install python2 -y
+       pkg install apache2 -y
+sleep 2
+   echo $purple"SELESAI"
+   echo
+fi
+if [ $grok = 2 ] || [ $grok = 02 ]
+then
+clear
+    sleep 1
+echo $green"# copyright by MR VIRUS SPM"
+echo $yellow"# sedang dimulai"
+    apt upgrade -y
+clear
+    apt install python -y
+clear
+    apt install python2 -y
+clear
+    apt install ruby -y
+clear
+    apt install toilet -y
+clear
+    apt install git -y
+clear
+    apt install figlet -y
+clear
+    gem install lolcat -y
+clear
+    apt install nano -y
+clear
+    pip install requests -y
+clear
+    pip2 install requests -y
+clear
+    pip install mechanize -y
+clear
+    pip2 install mechanize -y
+sleep 2
+    echo $purlle"OKEH GAN UDH SELESAI"
+fi
+if [ $grok = 3 ] || [ $grok = 03 ]
+then
+clear
+     pkg update & pkg upgrade -y
+     pkg install python -y
+     pkg install git -y
+     git clone https://github.com/karjok/terkey
+     sleep 1
+     cd terkey
+     sleep 1
+     python terkey.py
+fi
+if [ $grok = 4 ] || [ $grok = 04 ]
+then
+clear
+    pkg install git -y
+    pkg install python2 -y
+    git clone https://github.com/Inject-ID/tampilan-termux-v2
+sleep 1
+    cd tampilan-termux-v2
+    python2 run.py
+fi
